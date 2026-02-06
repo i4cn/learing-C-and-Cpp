dbabd07008d90cc2739ecc5f08d23e41cac7ed17
Bowen Zhang <127361113+i4cn@users.noreply.github.com>
2026-02-06 10:01:26 +0800

    Delete 1_BasicOutput_firstPGM.c

--- 1_BasicOutput_firstPGM.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>   // 包含标准输入输出库头文件，# 表示预处理
-int main()          // 定义主函数（一个程序有且只允许有一个main()函数
-{                   // main()函数的开始标志
-    printf("This is a C program.\n"); 
-                    // 调用输出函数printf()
-    return 0;       // 结束函数的运行，并向操作系统返回0
-}                   // main()函数的结束标志
